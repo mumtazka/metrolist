9b5a160817c9177df0d2a211c33271c137c0af29
MO AGAMY <mostafaalagamy@email.com>
2026-02-19 03:07:47 +0200

    Build: Bump to v13.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 141
-        versionName = "13.1.0"
+        versionName = "13.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
